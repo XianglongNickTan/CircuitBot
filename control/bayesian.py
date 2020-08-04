c091f592e48071ebae6061f8b56a2e7ceab9128e
XianglongNickTan <384894136@qq.com>
2020-08-04 12:26:37 +0800

    mkdir

--- control/bayesian.py
+++ control/bayesian.py
@@ -4,7 +4,7 @@ import time
 import pandas as pd
 from bayes_opt import BayesianOptimization
 from bayes_opt import UtilityFunction
-import keyboard
+# import keyboard
 # from bayes_opt import SequentialDomainReductionTransformer
 
 
@@ -15,7 +15,7 @@ import keyboard
 def circuitBot():
     optimizer = BayesianOptimization(
         f=None,
-        pbounds={'x': (-0.3, 0.2), 'y': (-0.5, -0.2)},
+        pbounds={'x1': (-0.15, 0.05), 'y1': (-0.55, -0.4), 'x2': (-0.15, 0.05), 'y2': (-0.55, -0.4), 'x3': (-0.15, 0.05), 'y3': (-0.55, -0.4)},
         verbose=2,  # choices: 0, 1, 2
         random_state=1,
         # bounds_transformer = SequentialDomainReductionTransformer()
@@ -24,17 +24,17 @@ def circuitBot():
     # For kind = "ei", small xi (0.0) prefer exploitation, big xi (0.0) prefer exploration
     # kind = "poi", small xi (0.0) prefer exploitation, big xi (0.0) prefer exploration
 
-    utility = UtilityFunction(kind="ucb", kappa=10, xi=0.0)
+    utility = UtilityFunction(kind="ucb", kappa=2, xi=0.0)
     
     # To output paramters and voltage as a csv file
-    name = ['x', 'y', 'voltage']
+    name = ['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'voltage']
     csv_list = []
 
     # train part
     for _ in range(10):
         next_to_probe = optimizer.suggest(utility) # A dict to tell you the next parameters to probe
 
-        point_file = open("point.txt", "w")
+        point_file = open("next.txt", "w")
         point_str = str()
         for key in next_to_probe:
             point_str += str(next_to_probe[key])
@@ -43,15 +43,18 @@ def circuitBot():
         point_file.write(point_str)
         point_file.close()
 
-        print("Now, change to the other termial to let robot arm draw.")
-        print("Press q when it finished.")
+        print("Now, change to the other terminal to let robot arm draw.")
+        # print("Press q when it finished.")
 
         ###################################################
         # Now we should wait the robot arm draw circle
         # Pass q when the robot arm finished
         ###################################################
 
-        keyboard.wait("q")
+        # keyboard.wait("q")
+        while True:
+            if input("Press q when it finished:") == "q":
+                break
 
         voltage_file = open("voltage.txt", "r")
         voltage = voltage_file.read()
@@ -70,7 +73,7 @@ def circuitBot():
         tmp.append(float(voltage))
         csv_list.append(tmp)
 
-    csv = pd.DataFrame(columns = name, data = csv_list)
+    csv = pd.DataFrame(columns=name, data=csv_list)
     output_file = "data.csv"
     csv.to_csv(output_file)
 
