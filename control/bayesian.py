8229fc9465d1aee4b280eec012476730d401772b
Lyu <lvwj@shanghaitech.edu.cn>
2020-08-12 16:01:00 +0800

    Aug 12th

--- control/bayesian.py
+++ control/bayesian.py
@@ -2,27 +2,38 @@
 import os
 import time
 import pandas as pd
+import random
 from bayes_opt import BayesianOptimization
 from bayes_opt import UtilityFunction
-# import keyboard
+from bayes_opt.logger import JSONLogger
+from bayes_opt.event import Events
+from bayes_opt.util import load_logs
+
 # from bayes_opt import SequentialDomainReductionTransformer
 
+# Bound region of parameter space
+# For now, we have 6 parameters. The range of x is (-0.21, 0.1), the range of y is (-0.55, -0.4)
+xybounds = {'x1': (-0.18, 0.12), 'y1': (-0.6, -0.45), 'x2': (-0.18, 0.12), 'y2': (-0.6, -0.45), 'x3': (-0.18, 0.12), 'y3': (-0.6, -0.45)}
+# Times of experiment
+times_of_experiments = 10
 
-# from control import MoveItIkDemo
 
 
 
 def circuitBot():
     optimizer = BayesianOptimization(
         f=None,
-        pbounds={'x1': (-0.21, 0.1), 'y1': (-0.55, -0.4), 'x2': (-0.21, 0.1), 'y2': (-0.55, -0.4), 'x3': (-0.21, 0.1), 'y3': (-0.55, -0.4)},
-        verbose=2,  # choices: 0, 1, 2
-        random_state=1,
+        pbounds=xybounds,
+        verbose=2,  # choices: 0, 1, 2. verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+        random_state=10,
         # bounds_transformer = SequentialDomainReductionTransformer()
     )
     # For kind = "ucb", small kappa (1) prefer exploitation, big kappa (10) prefer exploration
     # For kind = "ei", small xi (0.0) prefer exploitation, big xi (0.0) prefer exploration
     # kind = "poi", small xi (0.0) prefer exploitation, big xi (0.0) prefer exploration
+
+    logger = JSONLogger(path="./Logs.json")
+    optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
 
     utility = UtilityFunction(kind="ucb", kappa=2, xi=0.0)
     
@@ -30,11 +41,29 @@ def circuitBot():
     name = ['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'voltage']
     csv_list = []
 
+
+    # pretrainning part
+    probe1 = {'x1': 0.12, 'y1': -0.52, 'x2': -0.03, 'y2': -0.52, 'x3': -0.18, 'y3': -0.52}
+    probe2 = {'x1': 0.07, 'y1': -0.52, 'x2': -0.03, 'y2': -0.52, 'x3': -0.13, 'y3': -0.52}
+    probe3 = {'x1': 0.04, 'y1': -0.52, 'x2': -0.03, 'y2': -0.52, 'x3': -0.10, 'y3': -0.52}
+    probe4 = {'x1': 0.12, 'y1': -0.60, 'x2': -0.03, 'y2': -0.60, 'x3': -0.18, 'y3': -0.60}
+    probe5 = {'x1': 0.07, 'y1': -0.60, 'x2': -0.03, 'y2': -0.60, 'x3': -0.13, 'y3': -0.60}
+    probe6 = {'x1': 0.04, 'y1': -0.60, 'x2': -0.03, 'y2': -0.60, 'x3': -0.10, 'y3': -0.60}
+    probe7 = {'x1': 0.12, 'y1': -0.45, 'x2': -0.03, 'y2': -0.45, 'x3': -0.18, 'y3': -0.45}
+    probe8 = {'x1': 0.07, 'y1': -0.45, 'x2': -0.03, 'y2': -0.45, 'x3': -0.13, 'y3': -0.45}
+    probe9 = {'x1': 0.04, 'y1': -0.45, 'x2': -0.03, 'y2': -0.45, 'x3': -0.10, 'y3': -0.45}
+    probes = [probe1, probe2, probe3, probe4, probe5, probe6, probe7, probe8, probe9]
+    results = [8.7, 9.0, 8.4, 6.1, 6.1, 6.1, 6.1, 6.1, 6.1]
+    for j in range(9):
+        optimizer.register(params=probes[j], target=results[j])
+
     # train part
-    for _ in range(10):
+    for i in range(20):
         next_to_probe = optimizer.suggest(utility) # A dict to tell you the next parameters to probe
         print(next_to_probe)
-
+        # 将连续值变为离散值
+        for item in next_to_probe:
+            next_to_probe[item] = round(next_to_probe[item], 2)
         point_file = open("next.txt", "w")
         point_str = str()
         for key in next_to_probe:
@@ -64,11 +93,17 @@ def circuitBot():
         voltage_file.close()
         os.remove("voltage.txt")
 
-        optimizer.register(
-            params=next_to_probe,
-            target=float(voltage)
-        )
-
+        try:
+            optimizer.register(
+                params=next_to_probe,
+                target=float(voltage)
+            )
+        except KeyError:
+            next_to_probe['x1'] = next_to_probe['x1'] + random.randint(1, 1000)*0.001
+            optimizer.register(
+                params=next_to_probe,
+                target=float(voltage)
+            )
         # To record these parameter and voltage
         tmp = []
         for key in next_to_probe:
---
+++ control/control.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+import rospy, sys
+import moveit_commander
+from moveit_commander import PlanningSceneInterface
+from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import RobotState, Constraints, OrientationConstraint
+import math
+import numpy as np
+import copy
+import serial
+import os
+
+
+arduino_motor = serial.Serial('/dev/ttyACM0', 38400)
+# arduino_voltage = serial.Serial('/dev/ttyACM1', 9600, timeout=0.5)
+
+
+
+class MoveItIkDemo:
+	def __init__(self):
+		# 初始化move_group的API
+		moveit_commander.roscpp_initialize(sys.argv)
+
+		# 初始化ROS节点
+		rospy.init_node('real_jaco_moveit_fixed_grasp')
+
+		# 初始化需要使用move group控制的机械臂中的self.arm group
+		self.arm = moveit_commander.MoveGroupCommander('arm')
+
+		# 初始化需要使用move group控制的机械臂中的gripper group
+		# self.gripper = moveit_commander.MoveGroupCommander('gripper')
+
+		# 获取终端link的名称
+		self.end_effector_link = self.arm.get_end_effector_link()
+
+		# 设置目标位置所使用的参考坐标系
+		self.reference_frame = 'world'
+		self.arm.set_pose_reference_frame(self.reference_frame)
+
+
+		# 当运动规划失败后，不允许重新规划
+		self.arm.allow_replanning(False)
+
+		# 设置位置(单位：米)和姿态（单位：弧度）的允许误差
+		self.arm.set_goal_position_tolerance(0.001)
+		self.arm.set_goal_orientation_tolerance(0.001)
+		# self.gripper.set_goal_joint_tolerance(0.001)
+
+		# 设置允许的最大速度和加速度
+		self.arm.set_max_acceleration_scaling_factor(0.5)
+		self.arm.set_max_velocity_scaling_factor(0.5)
+
+		# 初始化场景对象
+		scene = PlanningSceneInterface()
+		rospy.sleep(1)
+
+		# ws = [-0.3, -0.6, 0.01, 0.2, -0.3, 0.1]
+		# self.arm.set_workspace(ws)
+
+		# # 控制机械臂先回到初始化位置，手爪打开
+		# self.arm.set_named_target('Home')
+		# self.arm.go()
+		# # self.gripper.set_named_target('Open')
+		# # self.gripper.go()
+		# rospy.sleep(1)
+
+		self.line_cont = 80
+		self.circle_cont = 40
+		self.radius = 0.10
+
+		self.target_pose = PoseStamped()
+		self.target_pose.header.frame_id = self.reference_frame
+		self.target_pose.pose.position.z = 0.03
+
+		self.target_pose.pose.orientation.x = 0.14578
+		self.target_pose.pose.orientation.y = 0.98924
+		self.target_pose.pose.orientation.z = -0.0085346
+		self.target_pose.pose.orientation.w = 0.0084136
+
+	# def init_upright_path_constraints(self, pose):
+	#
+	# 	self.upright_constraints = Constraints()
+	# 	self.upright_constraints.name = "upright"
+	# 	orientation_constraint = OrientationConstraint()
+	# 	orientation_constraint.header = pose.header
+	# 	orientation_constraint.link_name = self.end_effector_link
+	# 	orientation_constraint.orientation = pose.pose.orientation
+	# 	orientation_constraint.absolute_x_axis_tolerance = 0.2
+	# 	orientation_constraint.absolute_y_axis_tolerance = 0.2
+	# 	orientation_constraint.absolute_z_axis_tolerance = 0.2
+	# 	# orientation_constraint.absolute_z_axis_tolerance = 3.14 #ignore this axis
+	# 	orientation_constraint.weight = 1
+	#
+	# 	self.upright_constraints.orientation_constraints.append(orientation_constraint)
+	#
+	# def enable_upright_path_constraints(self):
+	# 	self.arm.set_path_constraints(self.upright_constraints)
+	#
+	# def disable_upright_path_constraints(self):
+	# 	self.arm.set_path_constraints(None)
+
+	def move_to(self, point):
+		self.target_pose.header.stamp = rospy.Time.now()
+		self.target_pose.pose.position.x = point[0]
+		self.target_pose.pose.position.y = point[1]
+		self.arm.set_start_state_to_current_state()
+		self.arm.set_pose_target(self.target_pose, self.end_effector_link)
+		traj = self.arm.plan()
+		self.arm.execute(traj)
+
+	def draw_line(self, xy_init_pos=None):
+		waypoints = []
+		# if xy_end_pos is None:
+		# 	xy_end_pos = [-0.2, -0.25]
+
+		if xy_init_pos is None:
+			xy_init_pos = [0.20, -0.52]
+
+
+		self.target_pose.header.stamp = rospy.Time.now()
+		self.target_pose.pose.position.x = xy_init_pos[0]
+		self.target_pose.pose.position.y = xy_init_pos[1]
+		self.arm.set_start_state_to_current_state()
+		self.arm.set_pose_target(self.target_pose, self.end_effector_link)
+		traj = self.arm.plan()
+		self.arm.execute(traj)
+
+		wpose = self.arm.get_current_pose()
+		wpose.pose.orientation.x = 0.14578
+		wpose.pose.orientation.y = 0.98924
+		wpose.pose.orientation.z = -0.00853
+		wpose.pose.orientation.w = 0.00841
+		wpose.pose.position.z = 0.03
+
+		# self.init_upright_path_constraints(wpose)
+		# self.enable_upright_path_constraints()
+
+		for t in range(self.line_cont):
+			wpose.pose.position.x = (xy_init_pos[0] - 0.46 / self.line_cont * (t + 1))
+			wpose.pose.position.y = xy_init_pos[1]
+			waypoints.append(copy.deepcopy(wpose.pose))
+
+		(plan, fraction) = self.arm.compute_cartesian_path(
+			waypoints,
+			0.005,             # SUPER IMPORTANT PARAMETER FOR VELOCITY CONTROL !!!!!
+			0.0
+		)
+		arduino_motor.write('2')
+		self.arm.execute(plan)
+		arduino_motor.write('0')
+		self.arm.stop()
+
+		# self.disable_upright_path_constraints()
+
+		rospy.sleep(10)
+
+
+	def draw_circle(self, xy_center_pos, radius = 0.05):
+		waypoints = []
+		# rospy.rostime.wallsleep(0.05)
+
+		self.target_pose.header.stamp = rospy.Time.now()
+		self.target_pose.pose.position.x = xy_center_pos[0] + radius
+		self.target_pose.pose.position.y = xy_center_pos[1]
+		self.arm.set_start_state_to_current_state()
+		self.arm.set_pose_target(self.target_pose, self.end_effector_link)
+		traj = self.arm.plan()
+		self.arm.execute(traj)
+		rospy.sleep(2)
+
+
+		wpose = self.arm.get_current_pose()
+		wpose.pose.orientation.x = 0.14578
+		wpose.pose.orientation.y = 0.98924
+		wpose.pose.orientation.z = -0.00853
+		wpose.pose.orientation.w = 0.00841
+		wpose.pose.position.z = 0.03
+
+		for _ in range (2):
+			for t in range(self.circle_cont):
+				wpose.pose.position.x = xy_center_pos[0] + radius * np.cos(2 * np.pi * (t+1) / self.circle_cont)
+				wpose.pose.position.y = xy_center_pos[1] + radius * np.sin(2 * np.pi * (t+1) / self.circle_cont)
+				waypoints.append(copy.deepcopy(wpose.pose))
+
+		# self.init_upright_path_constraints(wpose)
+		# self.enable_upright_path_constraints()
+
+		(plan, fraction) = self.arm.compute_cartesian_path(
+			waypoints,
+			# 0.0005,             # SUPER IMPORTANT PARAMETER FOR VELOCITY CONTROL !!!!!
+			0.01,  # SUPER IMPORTANT PARAMETER FOR VELOCITY CONTROL !!!!!
+			0.0
+		)
+
+		arduino_motor.write('1')
+		self.arm.execute(plan)
+		arduino_motor.write('0')
+		self.arm.stop()
+		rospy.sleep(2)
+
+		# self.disable_upright_path_constraints()
+		# rospy.sleep(2)
+
+
+		# moveit_commander.roscpp_shutdown()
+		# moveit_commander.os._exit(0)
+
+def test(xy_center_pos):
+	######################## For testing ########################
+
+	voltage = xy_center_pos[0] - xy_center_pos[1]**2 - xy_center_pos[2]**2 + xy_center_pos[3] + xy_center_pos[4]**2 - xy_center_pos[5]
+	voltage_file = open("voltage.txt", "w")
+	# print ("-------------------------")
+	# print ("file name: ", voltage_file.name)
+	# print ("-------------------------")
+	voltage_file.write(str(voltage))
+	voltage_file.close()
+	print("voltage:", voltage)
+
+######################## For testing ########################
+
+
+if __name__ == "__main__":
+	demo = MoveItIkDemo()
+	#
+	demo.draw_line()
+
+	###########################################################################
+	points_file = open("next.txt", "r")
+	points_str = points_file.read()
+	print(points_str)
+	points_list = points_str.split()
+	points_file.close()
+
+	#################################################################
+
+	center1 = [float(points_list[0]), float(points_list[3])]
+	center2 = [float(points_list[1]), float(points_list[4])]
+	center3 = [float(points_list[2]), float(points_list[5])]
+	demo.draw_circle(center1)
+	demo.draw_circle(center2)
+	demo.draw_circle(center3)
+
+	# demo.draw_circle([-0.08, -0.6])
+
+	# demo.draw_circle([0.12, -0.52])
+	# demo.draw_circle([-0.03, -0.52])
+	# demo.draw_circle([-0.18, -0.52])
+	# #
+	# demo.draw_circle([0.07, -0.52])
+	# demo.draw_circle([-0.03, -0.52])
+	# demo.draw_circle([-0.13, -0.52])
+	#
+	# demo.draw_circle([0, -0.45])
+	# demo.draw_circle([-0.03, -0.52])
+	# demo.draw_circle([-0.1, -0.52])
+
+	moveit_commander.roscpp_shutdown()
+	moveit_commander.os._exit(0)
+
+	# demo.draw_line()
+	#
+	# rospy.sleep(10)
+	# ###########################################################################
+	# points_file = open("next.txt", "r")
+	# points_str = points_file.read()
+	# # print(point_str)
+	# points_list = points_str.split()
+	# points_file.close()
+	# # os.remove("point.txt")
+	# # print(point_list)
+	#
+	# #################################################################
+	#
+	# center1 = [float(points_list[0]), float(points_list[3])-0.05]
+	# center2 = [float(points_list[1]), float(points_list[4])-0.05]
+	# center3 = [float(points_list[2]), float(points_list[5])-0.05]
+	# demo.draw_circle(center1)
+	# demo.draw_circle(center2)
+	# demo.draw_circle(center3)
+
+	# demo.draw_circle([-0.2, -0.6])
